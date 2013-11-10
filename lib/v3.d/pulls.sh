4331384c8a7fa3309058948f38bac782884d153a
Masahito Yoshida <masahito@axsh.net>
2013-11-10 20:43:01 +0900

    pulls: add new.

--- lib/v3.d/pulls.sh
+++ lib/v3.d/pulls.sh
@@ -16,3 +16,18 @@ task_get() {
   call_api -X GET \
    $(base_uri)/repos/${owner}/${repo}/pulls/${number}
 }
+
+task_new() {
+  # http://developer.github.com/v3/pulls/#create-a-pull-request
+  local owner=$1 repo=$2
+
+  call_api -X POST --data @- \
+   $(base_uri)/repos/${owner}/${repo}/pulls <<-EOS
+	{
+	  "title": "${title}",
+	  "body": "${body}",
+	  "head": "${head}",
+	  "base": "${base}"
+	}
+	EOS
+}
